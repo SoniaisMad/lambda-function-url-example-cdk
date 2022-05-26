c1f253d94102fb13271f199ed24a70e045a8dbf7
SoniaIsMad <manoubisonia@gmail.com>
2022-05-26 15:06:21 +0200

    function url

--- lib/lambda-http-stack.ts
+++ lib/lambda-http-stack.ts
@@ -1,6 +1,6 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class LambdaHttpStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,9 +8,18 @@ export class LambdaHttpStack extends Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaHttpQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myFunction= new lambda.Function(this, 'LambdaHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'index.handler'                // file is "index", function is "handler"
+    });
+
+    const fnUrl = myFunction.addFunctionUrl({  // your function url
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+
+    new CfnOutput(this, 'TheUrl', {
+      value: fnUrl.url,
+    });
   }
 }
